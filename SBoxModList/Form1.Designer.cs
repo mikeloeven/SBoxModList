4a07f35cc381beaedcf452d14186bdec18de74e3
mikeloeven <spamboxml2@gmail.com>
2014-11-20 03:22:10 -0500

    SBoxModList 0.1

---
+++ SBoxModList/Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace SBoxModList
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtOut = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(97, 28);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(75, 23);
+            this.btnOpen.TabIndex = 1;
+            this.btnOpen.Text = "Open File";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(110, 71);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Mod List";
+            // 
+            // txtOut
+            // 
+            this.txtOut.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtOut.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.txtOut.Location = new System.Drawing.Point(12, 141);
+            this.txtOut.Name = "txtOut";
+            this.txtOut.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.ForcedBoth;
+            this.txtOut.Size = new System.Drawing.Size(462, 158);
+            this.txtOut.TabIndex = 3;
+            this.txtOut.Text = "";
+            this.txtOut.WordWrap = false;
+            this.txtOut.LinkClicked += new System.Windows.Forms.LinkClickedEventHandler(this.txtOut_LinkClicked);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(486, 299);
+            this.Controls.Add(this.txtOut);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnOpen);
+            this.Name = "Form1";
+            this.Text = "SBCModView";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox txtOut;
+    }
+}
+
---
+++ SBoxModList/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Xml;
+using System.Xml.XPath;
+using System.Net;
+using System.Diagnostics;
+using System.Text.RegularExpressions;
+namespace SBoxModList
+{
+    public partial class Form1 : Form
+    {
+        OpenFileDialog ofd = new OpenFileDialog();
+        WebClient web = new WebClient();
+        XPathDocument SBCFile;
+        XPathNavigator nav;
+        XPathNodeIterator modListIt;
+        
+        public Form1()
+        {
+            ofd.Filter = "SBC|*.sbc";
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+
+                    
+                    String fName = ofd.FileName;
+                    String sFName = ofd.FileName;
+                    SBCFile = new XPathDocument(fName);
+                    nav = SBCFile.CreateNavigator();
+                    modListIt = nav.Select("//ModItem/PublishedFileId");
+
+                    while (modListIt.MoveNext())
+                    {
+                        StringBuilder outputStr = new StringBuilder();
+                        string ID = modListIt.Current.InnerXml;
+                        outputStr.Append("ModID>>   " + ID);
+                        string URL =  "http://steamcommunity.com/sharedfiles/filedetails/?id=" + ID;
+                        try{
+                            string workshopPage = web.DownloadString(URL);
+                        string title = Regex.Match(workshopPage, @"\<title\b[^>]*\>\s*(?<Title>[\s\S]*?)\</title\>", RegexOptions.IgnoreCase).Groups["Title"].Value;
+                        title = title.Replace("Steam Workshop :: ", "");
+                        outputStr.Append("  >>NAME>>    " + title);
+                        }
+                        catch(Exception)
+                        {
+                            outputStr.Append("      >>NAME>>         BadURL");
+                        }
+                        outputStr.Append("  >>LINK>>    " + URL);
+                        outputStr.Append("\r\n");
+                        txtOut.AppendText(outputStr.ToString());
+                                                
+                    }
+
+                    
+
+
+
+
+
+
+
+                }
+            }
+            catch (Exception)
+            {
+
+            }
+      
+
+        }
+
+        private void txtOut_LinkClicked(Object sender, LinkClickedEventArgs e)
+        {
+            Process.Start(e.LinkText);
+        }
+
+    }
+}
